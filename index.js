2a2f8f5912f26910ace69f53291a816be6e19efd
Dmitry <df.creative@gmail.com>
2016-05-08 21:50:54 -0400

    Update implementation

--- index.js
+++ index.js
@@ -5,9 +5,8 @@
  */
 
 var inherits = require('inherits');
-var Transform = require('stream').Transform;
+var Through = require('audio-through');
 var extend = require('xtend/mutable');
-var util = require('pcm-util');
 
 
 /**
@@ -16,51 +15,27 @@ var util = require('pcm-util');
 function AudioSlice (duration) {
 	if (!(this instanceof AudioSlice)) return new AudioSlice(duration);
 
-	Transform.call(this);
-
 	if (typeof duration === 'number') {
 		duration = {
 			duration: duration
 		}
 	}
 
-	extend(this, duration);
-
-	//precalculate format variables
-	util.normalizeFormat(this);
-
-	this.count = 0;
+	Through.call(this, duration);
 };
 
-inherits(AudioSlice, Transform);
+inherits(AudioSlice, Through);
 
 
 /** Default duration to slice is undefined */
 AudioSlice.prototype.duration = Infinity;
 
 
-/** Transformer */
-AudioSlice.prototype._transform = function (chunk, enc, cb) {
-	var frameLength = util.getFrameLength(chunk, this);
-
-	//increase generated data counter
-	this.count += frameLength;
-
-	// after getting "duration" second of audio, emit "end"
-	if (this.count >= this.sampleRate * this.duration) {
-		this.push(null);
+AudioSlice.prototype.process = function (chunk) {
+	if (this.count + chunk.length >= this.sampleRate * this.duration) {
+		this.end();
 	}
-	//send data
-	else {
-		this.push(chunk);
-	}
-
-	cb();
 }
-
-
-/** Extend default format */
-extend(AudioSlice.prototype, util.defaultFormat);
 
 
 
--- readme.md
+++ readme.md
@@ -1,11 +1,11 @@
-Slice input audio stream to the duration, i. e. sound after N seconds will not be played.
+Slice input audio stream to the duration, i. e. sound after N seconds will not be played and stream will end. Useful to force input stream to end.
 
 ## Usage
 
 [![npm install audio-slice](https://nodei.co/npm/audio-slice.png?mini=true)](https://npmjs.org/package/audio-slice/)
 
 ```js
-var Generator = require('audio-generator');
+var Sine = require('audio-oscillator/sine');
 var Slice = require('audio-slice');
 var Speaker = require('audio-speaker');
 
